f8d74238626c90e07d216cdfd229aca343d43495
alarst13 <ala22014@datasec.engr.uconn.edu>
2023-07-28 15:53:55 -0400

    .gitignore

--- rust/perlin/src/lib.rs
+++ rust/perlin/src/lib.rs
@@ -28,9 +28,11 @@ fn perlin_noise(
     ))
 }
 
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
+/*
+A Python module implemented in Rust. The name of this function must match
+the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+import the module.
+*/
 #[pymodule]
 fn perlin(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(perlin_noise, m)?)?;
