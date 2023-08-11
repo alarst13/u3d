e5e96888c1ea175f8797eb8cbb18b1fa68e1e3af
alarst13 <ala22014@datasec.engr.uconn.edu>
2023-08-11 16:47:43 -0400

    boundry-effect: temporal shift

--- python/u3d-attack-C3D.py
+++ python/u3d-attack-C3D.py
@@ -11,7 +11,7 @@ device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
 
 
 # Define the power normalization function P(z) = sign(z) * |z|^alpha
-def power_normalization(z, alpha):
+def power_normalization(z, alpha=0.5):
     return torch.sign(z) * torch.abs(z) ** alpha
 
 
@@ -21,7 +21,7 @@ def center_crop(frame):
     return np.array(frame).astype(np.uint8)
 
 
-# Exctract intermediate features from the C3D model with the given clip
+# Extract intermediate features from the C3D model for the input clip
 def intermediate_features(model, clip):
     inputs = np.array(clip).astype(np.float32)
     inputs = np.expand_dims(inputs, axis=0)
@@ -36,8 +36,47 @@ def intermediate_features(model, clip):
     return intermediate_dict
 
 
-# Maximize the distance between original and perturbed videos
-def attack_objective(model, alpha, input_video_path, params):
+def temporal_transformation(perturbation, tau_shift):
+    """
+    Apply temporal transformation to the perturbation by shifting frames.
+
+    Parameters
+    ----------
+    perturbation : numpy.ndarray
+        Original U3D perturbation of shape (H, W, T), where H is the height, W is the width, and T is the number of frames.
+    tau_shift : int
+        The temporal shift value for the transformation.
+
+    Returns
+    -------
+    numpy.ndarray
+        Transformed U3D perturbation with frames shifted by tau_shift along the temporal axis.
+
+    Notes
+    -----
+    This function performs a temporal shift on the input perturbation by rolling frames along the temporal axis.
+    """
+    H, W, T = perturbation.shape
+    tau_shift = int(tau_shift)  # Ensure tau_shift is an integer
+    transformed_perturbation = np.zeros((H, W, T))
+    for t in range(T):
+        transformed_perturbation[:, :, t] = np.roll(
+            perturbation[:, :, t], tau_shift, axis=0)
+    return transformed_perturbation
+
+
+def attack_objective(model, input_video_path, params):
+    """
+    Calculate the attack objective using Particle Swarm Optimization (PSO).
+
+    Parameters:
+    model (torch.nn.Module): The pre-trained DNN model.
+    input_video_path (str): Path to the input video.
+    params (list): List of U3D parameters for the attack.
+
+    Returns:
+    float: The attack objective value.
+    """
     features_original = {}
     features_perturbed = {}
     noise_values_3D = []
@@ -46,13 +85,15 @@ def attack_objective(model, alpha, input_video_path, params):
     num_octaves, wavelength_x, wavelength_y, wavelength_t, color_period = params
     T = 5  # Number of frames for Perlin noise generation (adjust as needed)
     epsilon = 8.0  # Maximum perturbation allowed (epsilon) for the U3D attack
+    alpha = 0.5  # The alpha parameter for power normalization
+    I = 5  # Number of iterations for sampling.
 
     cap_original = cv2.VideoCapture(input_video_path)
 
     # Get the video properties
     frame_width = int(cap_original.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap_original.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
+
     # Generate the Perlin noise for the first T frames
     noise_values_3D = generate_noise(
         T, frame_height, frame_width, num_octaves, wavelength_x, wavelength_y, wavelength_t, color_period, epsilon)
@@ -62,57 +103,72 @@ def attack_objective(model, alpha, input_video_path, params):
     distances = []
     frame_num = 0
 
-    # Loop through the frames in the video
-    while True:
-        ret_original, frame_original = cap_original.read()
+    total_distance_expectation = 0.0
+    # Sample I random temporal transformations
+    for _ in range(I):
+        # Generate a random time index (tau) using a uniform distribution
+        tau = np.random.uniform(0, T)
+        t_sum = 0.0  # Initialize the sum of distances for this iteration
 
-        if not ret_original:
-            break
+        # Transform the noise values using the temporal transformation
+        transformed_perturbation = temporal_transformation(
+            noise_values_3D, tau)
 
-        # Add Perlin noise to the current frame using the precomputed noise values
-        t = frame_num % T  # Loop through the noise values
-        frame_perturbed = add_perlin_noise_to_frame(
-            frame_original, noise_values_3D[t])
+        # Loop through the frames in the video
+        while True:
+            ret_original, frame_original = cap_original.read()
 
-        # Preprocess the frames by resizing and center cropping
-        tmp_original = center_crop(cv2.resize(frame_original, (171, 128)))
-        tmp_perturbed = center_crop(cv2.resize(frame_perturbed, (171, 128)))
+            if not ret_original:
+                break
 
-        # Apply color normalization
-        tmp_original = tmp_original - np.array([[[90.0, 98.0, 102.0]]])
-        tmp_perturbed = tmp_perturbed - np.array([[[90.0, 98.0, 102.0]]])
+            # Add Perlin noise to the current frame using the precomputed noise values
+            t = frame_num % T  # Loop through the noise values
+            frame_perturbed = add_perlin_noise_to_frame(
+                frame_original, transformed_perturbation[t])
 
-        clip_original.append(tmp_original)
-        clip_perturbed.append(tmp_perturbed)
+            # Preprocess the frames by resizing and center cropping
+            tmp_original = center_crop(cv2.resize(frame_original, (171, 128)))
+            tmp_perturbed = center_crop(
+                cv2.resize(frame_perturbed, (171, 128)))
 
-        if len(clip_original) == 16:
-            features_original = intermediate_features(
-                model, clip_original)
-            features_perturbed = intermediate_features(
-                model, clip_perturbed)
+            # Apply color normalization
+            tmp_original = tmp_original - np.array([[[90.0, 98.0, 102.0]]])
+            tmp_perturbed = tmp_perturbed - np.array([[[90.0, 98.0, 102.0]]])
 
-            # Calculate the power normalized distances at each layer
-            for layer_name in features_original.keys():
-                distance = torch.norm(power_normalization(
-                    features_original[layer_name], alpha) - power_normalization(features_perturbed[layer_name], alpha), p=2)
-                distances.append(distance)
+            clip_original.append(tmp_original)
+            clip_perturbed.append(tmp_perturbed)
 
-            # Remove the oldest frame from each clip
-            clip_original.pop(0)
-            clip_perturbed.pop(0)
+            if len(clip_original) == 16:
+                features_original = intermediate_features(
+                    model, clip_original)
+                features_perturbed = intermediate_features(
+                    model, clip_perturbed)
 
-        frame_num += 1
+                # Calculate the power normalized distances at each layer
+                for layer_name in features_original.keys():
+                    distance = torch.norm(power_normalization(
+                        features_original[layer_name], alpha) - power_normalization(features_perturbed[layer_name], alpha), p=2)
+                    distances.append(distance)
+
+                # Remove the oldest frame from each clip
+                clip_original.pop(0)
+                clip_perturbed.pop(0)
+
+                t_sum += sum(distances).detach().cpu().item()
+
+            frame_num += 1
+            distances = []  # Reset the distances for memory efficiency
+
+        # Calculate the expectation of the total distance as the sum of distances across iterations divided by the number of iterations (I),
+        total_distance_expectation += t_sum / I
 
     # Release video file handles
     cap_original.release()
 
-    total_distance = sum(distances)
-    total_distance = total_distance.detach().cpu().item()
-
     print("Params:", params)
-    print("Total distance:", total_distance)
+    print("Total distance:", total_distance_expectation)
 
-    return total_distance
+    return total_distance_expectation
 
 
 if __name__ == '__main__':
@@ -156,8 +212,7 @@ if __name__ == '__main__':
             Negative total distance (PSO minimizes, so we negate for maximization).
         """
         params = [round(p) if i == 0 else p for i, p in enumerate(params)]
-        total_distance = attack_objective(
-            model, 0.5, Path.video(), params)
+        total_distance = attack_objective(model, Path.video(), params)
         # Negate because PSO minimizes, and we want to maximize the distance
         return -total_distance
 
